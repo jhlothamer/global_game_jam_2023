123a0f89858a5b2ad62c5826ead651a8456a5b85
Jason Lothamer <jhlothamer@gmail.com>
2023-02-01 18:32:25 -0500

    make moving obstacles 'bounce' back into level when hitting edge

--- game/scenes/environment/moving_obstacle.gd
+++ game/scenes/environment/moving_obstacle.gd
@@ -1,14 +1,54 @@
 extends "res://scenes/environment/obstacle.gd"
 
 export var speed = 10.0
+export var oob_reflection_max = 60.0
+
+onready var _oob_reflection_max = deg2rad(oob_reflection_max)
 
 
 var _direction := Vector2.RIGHT
+var _oob_rect: Rect2
+var _corners := []
+var _oob_center := Vector2.ZERO
 
 
 func _ready():
 	_direction = _direction.rotated(rand_range(-PI, PI))
+	var lvl: Level = ServiceMgr.get_service(Level)
+	if !lvl:
+		printerr("MovingObstacle: couldn't get level service")
+		set_physics_process(false)
+		return
+	lvl.get_out_of_bounds_rect_async(funcref(self, "_get_out_of_bounds_rect_callback"))
+
+
+func _get_out_of_bounds_rect_callback(oob_rect: Rect2) -> void:
+	_oob_rect = oob_rect
+	_oob_rect = _oob_rect.grow(-20.0)
+	_oob_center = _oob_rect.position + .5 * _oob_rect.size
+	_corners = [
+		_oob_rect.position, 
+		_oob_rect.end,
+		Vector2(_oob_rect.position.x, _oob_rect.end.y),
+		Vector2(_oob_rect.end.x, _oob_rect.position.y),
+	]
+
 
 
 func _physics_process(delta):
 	position += _direction * speed * delta
+	if _oob_rect.has_point(position):
+		return
+	var corner_distances_sq = {}
+	
+	var pos = position
+	for pt in _corners:
+		corner_distances_sq[pos.distance_squared_to(pt)] = pt
+	
+	var sorted_distances = corner_distances_sq.keys()
+	sorted_distances.sort()
+	var pt1 = corner_distances_sq[sorted_distances[0]]
+	var pt2 = corner_distances_sq[sorted_distances[1]]
+	var side_center = (pt1 + pt2) / 2.0
+	var v:Vector2 = _oob_center - side_center
+	_direction = v.rotated(rand_range(-_oob_reflection_max, _oob_reflection_max)).normalized()
--- game/scenes/levels/level_base.gd
+++ game/scenes/levels/level_base.gd
@@ -5,9 +5,10 @@ export (int, 1, 3) var number_of_starts_to_continue := 1
 export (String, MULTILINE) var intro_message_bbs := "[center]Good Luck![/center]"
 
 onready var _items:Node2D = $Items
-
+onready var _oob_ref_rect: ReferenceRect = $OutOfBoundsReferenceRect
 
 var _get_number_of_items_callbacks = []
+var _get_out_of_bounds_rect_callbacks = []
 
 
 func _enter_tree():
@@ -18,6 +19,8 @@ func _ready():
 	var number_of_items = _items.get_child_count()
 	for callback in _get_number_of_items_callbacks:
 		callback.call_func(number_of_items)
+	for callback in _get_out_of_bounds_rect_callbacks:
+		callback.call_func(_get_out_of_bounds_rect())
 
 
 func _register_service():
@@ -30,3 +33,13 @@ func get_number_of_items_async(callback: FuncRef) -> void:
 		return
 	_get_number_of_items_callbacks.append(callback)
 
+
+func _get_out_of_bounds_rect() -> Rect2:
+	return Rect2(_oob_ref_rect.rect_position, _oob_ref_rect.rect_size)
+
+
+func get_out_of_bounds_rect_async(callback: FuncRef) -> void:
+	if _oob_ref_rect:
+		callback.call_func(_get_out_of_bounds_rect())
+		return
+	_get_out_of_bounds_rect_callbacks.append(callback)
